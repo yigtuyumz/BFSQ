b116a72d260d9d9712b8696b6c96c0f196a08006
yigtuyumz <yigtuyumz@gmail.com>
2025-03-08 19:50:30 +0300

    add random character support

--- mapgen.pl
+++ mapgen.pl
@@ -7,15 +7,30 @@ die "program x y density" unless (scalar(@ARGV) == 3);
 
 my ($x, $y, $density) = @ARGV;
 
-print "$y.ox\n";
+my $empty;
+my $wall;
+my $fill;
+
+my %unique_chars;
+while (keys %unique_chars < 3) {
+	my $random_char = chr(int(rand(95)) + 32);
+	$unique_chars{$random_char} = 1;
+}
+
+my @chars = keys %unique_chars;
+$empty = $chars[0];
+$wall = $chars[1];
+$fill = $chars[2];
+
+print "$y$empty$wall$fill\n";
 
 for (my $i = 0; $i < $y; $i++) {
 	for (my $j = 0; $j < $x; $j++) {
 		if (int(rand($y) * 2) < $density) {
-			print "o";
+			print "$wall";
 		}
 		else {
-			print ".";
+			print "$empty";
 		}
 	}
 	print "\n";
