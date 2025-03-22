92efea7eefca0f2f1299827845e0f69466b8f03f
yigtuyumz <yigtuyumz@gmail.com>
2025-03-22 19:32:32 +0300

    feat: removed unique char feature in map generator

--- mapgen.pl
+++ mapgen.pl
@@ -11,16 +11,9 @@ my $empty;
 my $wall;
 my $fill;
 
-my %unique_chars;
-while (keys %unique_chars < 3) {
-	my $random_char = chr(int(rand(95)) + 32);
-	$unique_chars{$random_char} = 1;
-}
-
-my @chars = keys %unique_chars;
-$empty = $chars[0];
-$wall = $chars[1];
-$fill = $chars[2];
+$empty = chr(int(rand(95)) + 32);
+$wall = chr(int(rand(95)) + 32);
+$fill = chr(int(rand(95)) + 32);
 
 print "$y$empty$wall$fill\n";
 
